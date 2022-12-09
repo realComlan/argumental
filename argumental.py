7e87ebb19ddf27a0a53599dbecea5f34f3462555
Comlan <cmondayday@gmail.com>
2022-12-09 18:09:23 +0100

    committed something new

---
+++ argumental.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Solver:
+	def __init__(self):
+		self.args = dict()
+
+	def prepare_attack_matrix(self):
+		n=len(self.args)
+		self.attack_matrix=np.zeros(n,n)
+	def extensions(self):
+		# n le nombre de arguments
+		result=[]
+		for i in range(len(self.args)):
+			pass
+	def function_gamma(self):
+		# 
+		pass
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import argumental
 import re
+
+if __name__ == "__main__":
+	pass
