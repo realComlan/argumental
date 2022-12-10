e64d7813252e040529520f08c42f59811e35ab6c
Comlan <cmondayday@gmail.com>
2022-12-10 23:16:50 +0100

    added the basic structure

--- argumental.py
+++ argumental.py
@@ -1,17 +1,53 @@
 import numpy as np
+import pysat 
 
-class Solver:
+class SolverManager:
+	
 	def __init__(self):
-		self.args = dict()
+		pass
+class Solver:
+	pass
+
+class MatrixSolver(Solver):
+
+	def __init__(self):
+		self.arguments = dict()
+		self.attacks = np.array()
+
+	def add_argument(self, arg):
+		if str(arg) not in self.arguments:
+			self.arguments[str(arg)] = len(self.arguments)
+
+	def add_attack(self, a, b):
+		self.attacks = np.append(self.attacks, (a,b))
 
 	def prepare_attack_matrix(self):
-		n=len(self.args)
+		n=len(self.arguments)
 		self.attack_matrix=np.zeros(n,n)
-	def extensions(self):
-		# n le nombre de arguments
-		result=[]
-		for i in range(len(self.args)):
-			pass
-	def function_gamma(self):
-		# 
+	
+	def conflict_free_sets(self):
+		result=np.array()
+		n=len(self.arguments)
+		N=1<<(n-1)
+		for i in range(N):
+			ss=np.array()
+			for j in range(n):
+				if i&(1<<j):
+					ss=np.append(ss,[j])
+			result=np.append(result,[j])
+
+	def characteristic_function(self):
 		pass
+
+class SATBasedSolver(Solver):
+	
+	def __init__(self):
+		pass
+
+	def solve(self):
+		pass
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argumental
-import re
 
 if __name__ == "__main__":
-	pass
+	solverManager = SolverManager.get_instance()
+	solverManager.solve(method=Solver.SATBASED)
+	solverManager.solve(method=Solver.MATRIXBASED)
